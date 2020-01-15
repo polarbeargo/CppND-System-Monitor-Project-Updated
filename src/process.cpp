c58db0666071454eb73c277428e088d2b9eefb25
alice <bow1226@gmail.com>
2020-01-15 20:52:53 +0800

    feat: Implement returns
    Implement Return this process's ID, CPU utilization, the command that generated this process, memory utilization, the user (name) that generated this process, the age of this process.

--- src/process.cpp
+++ src/process.cpp
@@ -12,22 +12,22 @@ using std::vector;
 
 // TODO: Return this process's ID
 int Process::Process(int pid) : pid_(pid) {}
-int Process::Pid() { return pid_; }
+int Process::Pid() const { return pid_; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+float Process::CpuUtilization() const { return cpu_; }
 
 // TODO: Return the command that generated this process
-string Process::Command() { return string(); }
+string Process::Command() const { return LinuxParser::Command(Pid()); }
 
 // TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+string Process::Ram() const { return LinuxParser::Ram(Pid()); }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::User() const { return LinuxParser::User(Pid()); }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+long int Process::UpTime() const { return LinuxParser::UpTime(Pid()); }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
