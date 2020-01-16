4d181381e71ad8b78c013d1ecc0bd796326c3213
alice <bow1226@gmail.com>
2020-01-16 10:23:53 +0800

    feat: Implement return the aggregate CPU utilization

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,16 @@
 #include "processor.h"
+#include "linux_parser.h"
 
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() {
+  float utilization(0);
+  long idle_ticks = LinuxParser::IdleJiffies();
+  long active_tick = LinuxParser::ActiveJiffies();
+  long duration_activate(active_tick - cached_active_tick);
+  long duration_idle(idle_ticks - cached_idle_tick);
+  long duration {duration_activate + duration_idle};
+  utilization = static_cast<float>(duration_activate) / duration;
+  cached_active_tick = active_tick;
+  cached_idle_tick = idle_ticks;
+  return utilization;
+}
