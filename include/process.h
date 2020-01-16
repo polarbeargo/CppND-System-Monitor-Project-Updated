0bab823ed8662f03354f72b488ea9769ee10cadc
alice <bow1226@gmail.com>
2020-01-16 12:18:44 +0800

    feat: Declare private members

--- include/process.h
+++ include/process.h
@@ -9,15 +9,22 @@ It contains relevant attributes as shown below
 class Process {
  public:
   int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
+  std::string User() const;                      // TODO: See src/process.cpp
+  std::string Command() const;                   // TODO: See src/process.cpp
+  float CpuUtilization() const;                  // TODO: See src/process.cpp
+  void CpuUtilization(long activate_ticks, long system_ticks);
+  std::string Ram() const;                       // TODO: See src/process.cpp
+  long int UpTime() const;                       // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
-
+  bool operator>(const Process& a) const;
   // TODO: Declare any necessary private members
  private:
+  int pid_;
+  float cpu_{0};
+  long cached_activate_ticks{0};
+  long cached_idle_ticks{0};
+  long cached_system_tick{0};
+  
 };
 
 #endif
