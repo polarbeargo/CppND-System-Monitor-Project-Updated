2edeca128f767396b39f497459b04a2ffac66b7f
alice <bow1226@gmail.com>
2020-01-16 12:19:30 +0800

    style: Format

--- include/process.h
+++ include/process.h
@@ -8,13 +8,13 @@ It contains relevant attributes as shown below
 */
 class Process {
  public:
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User() const;                      // TODO: See src/process.cpp
-  std::string Command() const;                   // TODO: See src/process.cpp
-  float CpuUtilization() const;                  // TODO: See src/process.cpp
+  int Pid();                     // TODO: See src/process.cpp
+  std::string User() const;      // TODO: See src/process.cpp
+  std::string Command() const;   // TODO: See src/process.cpp
+  float CpuUtilization() const;  // TODO: See src/process.cpp
   void CpuUtilization(long activate_ticks, long system_ticks);
-  std::string Ram() const;                       // TODO: See src/process.cpp
-  long int UpTime() const;                       // TODO: See src/process.cpp
+  std::string Ram() const;                 // TODO: See src/process.cpp
+  long int UpTime() const;                 // TODO: See src/process.cpp
   bool operator<(Process const& a) const;  // TODO: See src/process.cpp
   bool operator>(const Process& a) const;
   // TODO: Declare any necessary private members
@@ -24,7 +24,6 @@ class Process {
   long cached_activate_ticks{0};
   long cached_idle_ticks{0};
   long cached_system_tick{0};
-  
 };
 
 #endif
