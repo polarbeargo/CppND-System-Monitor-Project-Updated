591f955a5a63acd7c304c481649cbfa81adf87cb
alice <bow1226@gmail.com>
2020-01-25 07:43:37 +0800

    fix: Declare cached_active_ticks_ and cached_system_ticks_

--- include/processor.h
+++ include/processor.h
@@ -4,9 +4,25 @@
 class Processor {
  public:
   float Utilization();  // TODO: See src/processor.cpp
+  Process(int pid);
+  int Pid() const;
+  std::string Command() const;
+  std::string Ram() const;
+  std::string User() const;
+  long int UpTime() const;
+  float CpuUtilization() const;
+  void CpuUtilization(long active_ticks, long system_ticks);
+  long Jiffies() const;
+  bool operator<(const Process& a) const;
+  bool operator>(const Process& a) const;
 
   // TODO: Declare any necessary private members
  private:
+  int pid_;
+  float cpu_{0};
+  long cached_active_ticks_{0};
+  long cached_idle_ticks_{0};
+  long cached_system_ticks_{0};
 };
 
 #endif
