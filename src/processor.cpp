ae9c8994614452c08fe7b5ce182f97b5bb974f0d
alice <bow1226@gmail.com>
2020-01-25 08:18:04 +0800

    fix: Typo

--- src/processor.cpp
+++ src/processor.cpp
@@ -6,8 +6,8 @@ float Processor::Utilization() {
   float utilization(0);
   long idle_ticks = LinuxParser::IdleJiffies();
   long active_tick = LinuxParser::ActiveJiffies();
-  long duration_activate(active_tick - cached_active_tick);
-  long duration_idle(idle_ticks - cached_idle_tick);
+  long duration_activate(active_tick - cached_active_ticks_);
+  long duration_idle(idle_ticks - cached_idle_ticks_);
   long duration {duration_activate + duration_idle};
   utilization = static_cast<float>(duration_activate) / duration;
   cached_active_tick = active_tick;
