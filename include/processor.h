589730d93b84cb59919148c3c9d12f3bf7389f6a
alice <bow1226@gmail.com>
2020-01-25 08:00:15 +0800

    style: clean code

--- include/processor.h
+++ include/processor.h
@@ -1,29 +1,15 @@
 #ifndef PROCESSOR_H
 #define PROCESSOR_H
 
-#include <string>
+#include <vector>
 
 class Processor {
- public:  // TODO: See src/processor.cpp
-  Process(int pid);
-  int Pid() const;
-  std::string Command() const;
-  std::string Ram() const;
-  std::string User() const;
-  long int UpTime() const;
-  float CpuUtilization() const;
-  void CpuUtilization(long active_ticks, long system_ticks);
-  long Jiffies() const;
-  bool operator<(const Process& a) const;
-  bool operator>(const Process& a) const;
+ public:
+  float Utilization();
 
-  // TODO: Declare any necessary private members
  private:
-  int pid_;
-  float cpu_{0};
   long cached_active_ticks_{0};
   long cached_idle_ticks_{0};
-  long cached_system_ticks_{0};
 };
 
 #endif
