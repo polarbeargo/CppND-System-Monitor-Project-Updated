055637610e1a0c9b186112e289d00c976c029ef6
alice <bow1226@gmail.com>
2020-01-26 12:41:21 +0800

    Revert "fix:Write std::replace(str.begin(),str.end(),':',' ');, then every ":" will be converted to " "(empty space)"
    This reverts commit d229ae69f0f6b4f6bd29fb7581292e3d28d56cd6.

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -260,8 +260,7 @@ string LinuxParser::User(int pid) {
     while (std::getline(stream, line)) {
       auto marker = line.find(token);
       if (marker != string::npos) {
-        std::replace(line.begin(),line.end(),':',' ');
-        return line;
+        return line.substr(0, marker - 1);
       }
     }
   }
