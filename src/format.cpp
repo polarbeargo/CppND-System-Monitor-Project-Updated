626a359ed99c47ce43d18d18f9257565b0d0f113
alice <bow1226@gmail.com>
2020-01-27 12:03:09 +0800

    fix: To pad values with a leading zero when they are less than 10

--- src/format.cpp
+++ src/format.cpp
@@ -8,12 +8,17 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
+std::string Format::Pad(string s, char c) {
+  s.insert(s.begin(), 2 - s.size(), c);
+  return s;
+}
+
 string Format::ElapsedTime(long seconds) {
-  long lseconds = int(seconds % 60);
+  long seconds = int(seconds % 60);
   long minutes = seconds / 60;
   long hours = minutes / 60;
   minutes = int(minutes % 60);
-  string result = std::to_string(hours) + ":" + std::to_string(minutes) + ":" +
-                  std::to_string(lseconds);
-  return result;
+  std::string(Format::Pad(to_string(hours), '0') + ":" +
+                     Format::Pad(to_string(minutes), '0') + ":" +
+                     Format::Pad(to_string(seconds), '0'));
 }
