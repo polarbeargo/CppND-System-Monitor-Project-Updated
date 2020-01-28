d5a0d448525ea3fccbec03dd3c208bfedee475e2
alice <bow1226@gmail.com>
2020-01-28 08:53:07 +0800

    refactor: function ElapsedTime()

--- include/format.h
+++ include/format.h
@@ -5,7 +5,7 @@
 
 namespace Format {
 std::string Pad(std::string s, char c);
-std::string ElapsedTime(long times);  // TODO: See src/format.cpp
-};                                    // namespace Format
+std::string ElapsedTime(long int time);  // TODO: See src/format.cpp
+};                                       // namespace Format
 
 #endif
--- src/format.cpp
+++ src/format.cpp
@@ -9,29 +9,16 @@ using std::to_string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds) {
-  long lseconds = int(seconds % 60);
-  long minutes = seconds / 60;
-  long hours = minutes / 60;
-  minutes = int(minutes % 60);
-  string display_hours = "";
-  if (hours < 10) {
-    display_hours = "0" + std::to_string(hours);
-  } else {
-    display_hours = std::to_string(hours);
-  }
-  string display_min = "";
-  if (minutes < 10) {
-    display_min = "0" + std::to_string(minutes);
-  } else {
-    display_min = std::to_string(minutes);
-  }
-  string display_sec = "";
-  if (lseconds < 10) {
-    display_sec = "0" + std::to_string(lseconds);
-  } else {
-    display_sec = std::to_string(lseconds);
-  }
-  string result = display_hours + ":" + display_min + ":" + display_sec;
-  return result;
+std::string Format::Pad(string s, char c) {
+  s.insert(s.begin(), 2 - s.size(), c);
+  return s;
+}
+
+string Format::ElapsedTime(long int time) {
+  int hours = time / (60 * 60);
+  int minutes = (time / 60) % 60;
+  long seconds = time % 60;
+  return std::string(Format::Pad(to_string(hours), '0') + ":" +
+                     Format::Pad(to_string(minutes), '0') + ":" +
+                     Format::Pad(to_string(seconds), '0'));
 }
